fc91fe46ae242374c25686f905e484a1b1a62995
Aleyda <aquispehach@unsa.edu.pe>
2025-10-24 17:17:47 -0500

    Ejercicio 3: Agregar contador de numeros

---
+++ Ejercicio3.js
@@ -0,0 +1,20 @@
+//EJERCICIO 3
+/* Contador de números pares e impares. Pide 10 números y muestra cuántos fueron pares y cuántos impares*/
+let edad = parseInt(prompt("Ingrese su edad:"));
+
+if (isNaN(edad) || edad < 0) {
+  alert("Edad inválida. Intente nuevamente.");
+} 
+else if (edad < 12) {
+  alert("Niño");
+} 
+else if (edad <= 17) {
+  alert("Adolescente");
+} 
+else if (edad <= 59) {
+  alert("Adulto");
+} 
+else {
+  alert("Adulto mayor");
+}
+console.log("Edad ingresada:", edad);
